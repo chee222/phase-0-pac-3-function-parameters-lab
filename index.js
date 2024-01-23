0fd9922fac7b868eebdc8352d9a86b56eb693e47
Chee <ck_white@outlook.com>
2024-01-22 19:44:17 -0500

    parameters2

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ function introductionWithLanguage(Samip, Ember, React) {
     return(`Hi, my name is ${Samip} and I am learning to program in ${React}.`)
   }
 
- 
   function introductionWithLanguageOptional(name, language = "JavaScript") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
   
-  // Test the function and store the result in variables
-  const result1 = introductionWithLanguageOptional("Gracie");// Hi, my name is Gracie and I am learning to program in JavaScript.
-  const result2 = introductionWithLanguageOptional("Gracie", "Python");// Hi, my name is Gracie and I am learning to program in Python.
+  const result1 = introductionWithLanguageOptional("Gracie");
+  console.log(result1); // Output: Hi, my name is Gracie and I am learning to program in JavaScript.
   
-function introductionWithLanguageOptional(Gracie, Python) {
-    return `Hi, my name is ${Gracie} and I am learning to program in ${Python}.`;
-  }
+  const result2 = introductionWithLanguageOptional("Gracie", "Python");
+  console.log(result2); // Output: Hi, my name is Gracie and I am learning to program in Python.
